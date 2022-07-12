bc7c71928a43582757e16adf03a4dc3489a8bf61
vincent <vincent.paturet@entur.org>
2022-07-12 15:16:17 +0200

    Upgrade Apache Commons Lang 3

--- mobi.chouette.model/src/main/java/mobi/chouette/model/NeptuneIdentifiedObject.java
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/NeptuneIdentifiedObject.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 import lombok.ToString;
 import mobi.chouette.model.util.ObjectIdTypes;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.NaturalId;
 import java.time.LocalDateTime;
 
