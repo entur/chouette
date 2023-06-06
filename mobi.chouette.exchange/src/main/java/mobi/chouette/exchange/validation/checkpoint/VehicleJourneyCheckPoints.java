85f5ce03f46004d9c89c03002f4d71571ed342d9
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-06-06 11:11:51 +0200

    Validating transport mode taxi for bus and coach stops.

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/checkpoint/VehicleJourneyCheckPoints.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/checkpoint/VehicleJourneyCheckPoints.java
@@ -786,12 +786,14 @@ public class VehicleJourneyCheckPoints extends AbstractValidation<VehicleJourney
 				(TransportModeNameEnum.Bus == vehicleJourneyTransportMode && TransportModeNameEnum.Coach == stopMode)) {
 			// Coach and bus are interchangeable
 			return true;
+		} else if (TransportModeNameEnum.Taxi == vehicleJourneyTransportMode && (stopMode == TransportModeNameEnum.Bus || stopMode == TransportModeNameEnum.Coach)) {
+			return true;
 		} else if (vehicleJourneyTransportMode != stopMode) {
 			return false;
 		} else if (TransportSubModeNameEnum.RailReplacementBus == stopSubMode && vehicleJourneyTransportSubMode != null && TransportSubModeNameEnum.RailReplacementBus != vehicleJourneyTransportSubMode) {
 			// Only rail replacement bus service can visit rail replacement bus stops
 			return false;
-		} else {
+		}  else {
 			return true;
 		}
 	}
